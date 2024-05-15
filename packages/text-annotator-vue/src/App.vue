182c847c69d9febb3a953ddfd72c0b0a7588bbc1
Joren Six <joren.six@ugent.be>
2024-05-15 13:24:17 +0200

    First vue component implementation

---
+++ packages/text-annotator-vue/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TextAnnotator from './components/TextAnnotator.vue'
+</script>
+
+<template>
+  <TextAnnotator msg="Vite + Vue" >
+    Text to annotate
+  </TextAnnotator>
+</template>
+
+<style scoped>
+
+</style>
---
+++ packages/text-annotator-vue/src/components/TextAnnotator.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import type { HighlightStyleExpression, TextAnnotation, TextAnnotatorOptions } from '@recogito/text-annotator';
+import { createTextAnnotator } from '@recogito/text-annotator';
+
+import '@recogito/text-annotator/dist/text-annotator.css';
+
+onMounted(() => {
+  const contentContainer = document.getElementById("to_annotate");
+  createTextAnnotator(contentContainer);
+});
+
+</script>
+
+<template>
+  <div id="to_annotate" ref="to_annotate"><slot /></div>
+</template>
+
+<style scoped>
+
+</style>
